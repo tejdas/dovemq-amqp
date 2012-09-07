7a17110097aee62e9c402ea863fccf1d82f7ac89
Tejeswar Das <tejeswar@gmail.com>
2012-09-06 22:44:45 -0700

    link dispositions work in progress

---
+++ dovemq/src/main/java/net/dovemq/transport/endpoint/CAMQPEndpointManager.java
@@ -0,0 +1,46 @@
+package net.dovemq.transport.endpoint;
+
+import net.dovemq.transport.link.CAMQPLinkEndpoint;
+import net.dovemq.transport.link.CAMQPLinkFactory;
+import net.dovemq.transport.link.CAMQPLinkManager;
+import net.dovemq.transport.link.CAMQPLinkReceiver;
+import net.dovemq.transport.link.CAMQPLinkSender;
+import net.dovemq.transport.link.LinkRole;
+
+public final class CAMQPEndpointManager
+{
+    public CAMQPSourceInterface createSource(String containerId, String source, String target)
+    {
+        CAMQPLinkSender linkSender = CAMQPLinkFactory.createLinkSender(containerId, source, target);
+        linkSender.setMaxAvailableLimit(16348);
+        CAMQPSource dovemqSource = new CAMQPSource(linkSender);
+        linkSender.setSource(dovemqSource);
+        return dovemqSource;
+    }
+    
+    public void createTarget()
+    {
+    }
+    
+    public CAMQPTargetInterface attachTarget(String linkSource, String linkTarget)
+    {
+        CAMQPLinkEndpoint linkEndpoint = CAMQPLinkManager.getLinkmanager().getLinkEndpoint(linkSource, linkTarget);
+        if (linkEndpoint == null)
+        {
+            System.out.println("could not find linkEndpoint");
+            return null;
+        }
+        if (linkEndpoint.getRole() == LinkRole.LinkReceiver)
+        {
+            CAMQPLinkReceiver linkReceiver = (CAMQPLinkReceiver) linkEndpoint;
+            CAMQPTarget dovemqTarget = new CAMQPTarget(linkReceiver);
+            linkReceiver.setTarget(dovemqTarget);
+            return dovemqTarget;
+        }
+        else
+        {
+            System.out.println("LinkEndpoint is not a LinkReceiver");           
+        }
+        return null;
+    }
+}
