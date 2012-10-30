c9d0a7875d306de5089317114276f0ba25e53e33
Tejeswar Das <tejeswar@gmail.com>
2012-10-29 22:30:04 -0700

    DoveMQ API work: QueueProcessor implementation: work-in-progress

---
+++ dovemq/src/main/java/net/dovemq/broker/endpoint/DoveMQEndpointManager.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2012 Tejeswar Das
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.dovemq.broker.endpoint;
+
+import net.dovemq.transport.endpoint.CAMQPSourceInterface;
+import net.dovemq.transport.link.CAMQPLinkReceiverInterface;
+
+public interface DoveMQEndpointManager
+{
+    public void publisherAttached(String queueName, CAMQPLinkReceiverInterface source);
+    public void publisherDetached(String queueName);
+    public void consumerAttached(String queueName, CAMQPSourceInterface target);
+    public void consumerDetached(String queueName);
+}
---
+++ dovemq/src/main/java/net/dovemq/broker/endpoint/DoveMQEndpointManagerImpl.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2012 Tejeswar Das
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.dovemq.broker.endpoint;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import net.dovemq.transport.endpoint.CAMQPSourceInterface;
+import net.dovemq.transport.link.CAMQPLinkReceiverInterface;
+
+class DoveMQEndpointManagerImpl implements DoveMQEndpointManager
+{
+    private final ConcurrentMap<String, QueueProcessor> queueProcessors = new ConcurrentHashMap<String, QueueProcessor>();
+
+    @Override
+    public void publisherAttached(String queueName, CAMQPLinkReceiverInterface source)
+    {
+        QueueProcessor queueProcessor = queueProcessors.get(queueName);
+        if (queueProcessor == null)
+        {
+            queueProcessor = new QueueProcessor();
+            queueProcessor.sourceAttached(source);
+            queueProcessors.put(queueName,  queueProcessor);
+        }
+        else
+        {
+            queueProcessor.sourceAttached(source);
+        }
+    }
+
+    @Override
+    public void publisherDetached(String queueName)
+    {
+        QueueProcessor queueProcessor = queueProcessors.get(queueName);
+        if (queueProcessor != null)
+        {
+            queueProcessor.sourceDetached();
+            if (queueProcessor.isCompletelyDetached())
+            {
+                queueProcessors.remove(queueName);
+            }
+        }
+    }
+
+    @Override
+    public void consumerAttached(String queueName, CAMQPSourceInterface target)
+    {
+        QueueProcessor queueProcessor = queueProcessors.get(queueName);
+        if (queueProcessor == null)
+        {
+            queueProcessor = new QueueProcessor();
+            queueProcessor.destinationAttached(target);
+            queueProcessors.put(queueName,  queueProcessor);
+        }
+        else
+        {
+            queueProcessor.destinationAttached(target);
+        }
+    }
+
+    @Override
+    public void consumerDetached(String queueName)
+    {
+        QueueProcessor queueProcessor = queueProcessors.get(queueName);
+        if (queueProcessor != null)
+        {
+            queueProcessor.destinationDetached();
+            if (queueProcessor.isCompletelyDetached())
+            {
+                queueProcessors.remove(queueName);
+            }
+        }
+    }
+}
