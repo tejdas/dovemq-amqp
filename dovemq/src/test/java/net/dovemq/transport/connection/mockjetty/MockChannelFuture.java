573e896d5e6605f7152eb9eb7a417b118a95fd96
Tejeswar Das <tejeswar@gmail.com>
2013-03-07 21:31:22 -0800

    fix warnings

--- dovemq/src/test/java/net/dovemq/transport/connection/mockjetty/MockChannelFuture.java
+++ dovemq/src/test/java/net/dovemq/transport/connection/mockjetty/MockChannelFuture.java
@@ -23,164 +23,141 @@ import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelFutureListener;
 
-public class MockChannelFuture implements org.jboss.netty.channel.ChannelFuture
-{
+public class MockChannelFuture implements ChannelFuture {
     private ChannelFutureListener listener = null;
+
     private boolean isLastFuture = false;
-    public boolean isLastFuture()
-    {
+
+    public boolean isLastFuture() {
         return isLastFuture;
     }
 
-    public void setLastFuture(boolean isLastFuture)
-    {
+    public void setLastFuture(boolean isLastFuture) {
         this.isLastFuture = isLastFuture;
     }
 
     @Override
-    public void addListener(ChannelFutureListener arg0)
-    {
-        synchronized (this)
-        {
+    public void addListener(ChannelFutureListener arg0) {
+        synchronized (this) {
             listener = arg0;
         }
     }
 
-    public synchronized ChannelFutureListener
-    getListener()
-    {
+    public synchronized ChannelFutureListener getListener() {
         return listener;
     }
 
     @Override
-    public ChannelFuture await() throws InterruptedException
-    {
+    public ChannelFuture await() throws InterruptedException {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public boolean await(long arg0) throws InterruptedException
-    {
+    public boolean await(long arg0) throws InterruptedException {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public boolean await(long arg0, TimeUnit arg1) throws InterruptedException
-    {
+    public boolean await(long arg0, TimeUnit arg1) throws InterruptedException {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public ChannelFuture awaitUninterruptibly()
-    {
+    public ChannelFuture awaitUninterruptibly() {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public boolean awaitUninterruptibly(long arg0)
-    {
+    public boolean awaitUninterruptibly(long arg0) {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public boolean awaitUninterruptibly(long arg0, TimeUnit arg1)
-    {
+    public boolean awaitUninterruptibly(long arg0, TimeUnit arg1) {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public boolean cancel()
-    {
+    public boolean cancel() {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public Throwable getCause()
-    {
+    public Throwable getCause() {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Channel getChannel()
-    {
+    public Channel getChannel() {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public boolean isCancelled()
-    {
+    public boolean isCancelled() {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public boolean isDone()
-    {
+    public boolean isDone() {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public boolean isSuccess()
-    {
+    public boolean isSuccess() {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public void removeListener(ChannelFutureListener arg0)
-    {
+    public void removeListener(ChannelFutureListener arg0) {
         // TODO Auto-generated method stub
 
     }
 
     @Override
-    public boolean setFailure(Throwable arg0)
-    {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean setSuccess()
-    {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean setProgress(long arg0, long arg1, long arg2)
-    {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public ChannelFuture rethrowIfFailed() throws Exception
-    {
+    @Deprecated
+    public ChannelFuture rethrowIfFailed() throws Exception {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public ChannelFuture sync() throws InterruptedException
-    {
+    public boolean setFailure(Throwable arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean setProgress(long arg0, long arg1, long arg2) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean setSuccess() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ChannelFuture sync() throws InterruptedException {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public ChannelFuture syncUninterruptibly()
-    {
+    public ChannelFuture syncUninterruptibly() {
         // TODO Auto-generated method stub
         return null;
     }
